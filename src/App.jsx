9c17b1a28ac905c332131f5c2da0d52f86ce6b27
Gabriel Henrique <ghbranco6@gmail.com>
2024-03-25 20:04:17 -0300

    🎉 Exercicio arrays em react
    Link do exercicio: https://www.origamid.com/slide/react-completo/#/0208-jsx-arrays/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,65 @@
-// Mostre os dados da aplicação, como apresentado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
 import "./style.css";
 
-const luana = {
-  cliente: "Luana",
-  idade: 27,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: "Mario",
-  idade: 31,
-  compras: [
-    { nome: "Notebook", preco: "R$ 2500" },
-    { nome: "Geladeira", preco: "R$ 3000" },
-    { nome: "Smartphone", preco: "R$ 1500" },
-    { nome: "Guitarra", preco: "R$ 3500" },
-  ],
-  ativa: false,
-};
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    preco: "R$ 2000",
+    cores: ["#29d8d5", "#252a34", "#fc3766"],
+  },
+  {
+    id: 2,
+    nome: "Notebook",
+    preco: "R$ 3000",
+    cores: ["#ffd045", "#d4394b", "#f37c59"],
+  },
+  {
+    id: 3,
+    nome: "Tablet",
+    preco: "R$ 1500",
+    cores: ["#365069", "#47c1c8", "#f95786"],
+  },
+];
 
 const App = () => {
-  // * Mudar dados que são mostrados
-  const dados = luana;
-
-  const precos = dados.compras.map((compra) => {
-    return parseInt(compra.preco.replace("R$ ", "")); //tira o cifrão para poder transformar em parseInt
-  });
-
-  const totalPrecos = precos.reduce((totalAtual, preco) => {
-    return preco + totalAtual;
-  }, 0);
-
   return (
     <>
-      <h1>Dados do cliente</h1>
-      <ul>
-        <li>
-          Nome: <b>{dados.cliente}</b>
-        </li>
-        <li>
-          Idade: <b> {dados.idade}</b>
-        </li>
-        <li>
-          Situação:
-          <b className={dados.ativa ? "sucesso" : "perigo"}>
-            {dados.ativa ? "Conta ativa!" : "Conta desativada!"}
-          </b>
-        </li>
-        <li>
-          Total gasto:
-          <b className={totalPrecos >= 10000 ? "perigo" : "sucesso"}>
-            {totalPrecos}
-          </b>
-        </li>
-      </ul>
-      <b className="perigo">
-        {" "}
-        {totalPrecos >= 10000 && "Gasto excedido. Conta desativada!"}{" "}
-      </b>
-
-      <h4>Suas compras foram:</h4>
-      <ul>
-        {dados.compras.map(({ nome, preco }) => (
-          <li key={nome}>
-            Item: {nome}. Valor: {preco}
-          </li>
-        ))}
-      </ul>
-
-      <i>Para mudar os valores exibidos, altere a linha 33</i>
+      <h1>
+        <a
+          target="_blank"
+          href="https://www.origamid.com/slide/react-completo/#/0208-jsx-arrays/5"
+        >
+          Slide 0208 ↗
+        </a>
+      </h1>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace("R$ ", "")) > 1500)
+        .map(
+          (
+            { id, nome, preco, cores } // Seria o mesmo que .map((produto)), porém iria repetir produto toda vez que fosse chamar o id, nome preco...
+          ) => (
+            <div key={id}>
+              <h2>Produto: {nome}</h2>
+              <div>Preço: R$ {Number(preco.replace("R$ ", ""))}</div>
+              <ul>
+                {cores.map((cor) => (
+                  <li
+                    key={cor}
+                    style={{
+                      backgroundColor: cor,
+                      padding: ".5em",
+                      color: "#fff",
+                    }}
+                  >
+                    Cor de background: {cor.toUpperCase()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
+        )}
     </>
   );
 };
