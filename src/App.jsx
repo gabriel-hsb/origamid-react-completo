40c5b280a76a6160a99da9bd60625cc57c84ba29
Gabriel Henrique <ghbranco6@gmail.com>
2024-03-25 20:32:34 -0300

    ✨ Feat (app.jsx) event listeners in jsx
    Link do slide: https://www.origamid.com/slide/react-completo/#/0209-eventos/1

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,30 @@
 // Mostre apenas produtos que forem mais caros que R$ 1500
 import "./style.css";
 
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
-
 const App = () => {
+  function handleClick(e) {
+    alert(`Você comprou ${e.target.innerText}`);
+  }
+  function handleHover() {
+    alert("Você passou o mouse! ");
+  }
   return (
     <>
       <h1>
         <a
           target="_blank"
-          href="https://www.origamid.com/slide/react-completo/#/0208-jsx-arrays/5"
+          href="https://www.origamid.com/slide/react-completo/#/0209-eventos/1"
         >
-          Slide 0208 ↗
+          Slide 0209 ↗
         </a>
       </h1>
-      {produtos
-        .filter(({ preco }) => Number(preco.replace("R$ ", "")) > 1500)
-        .map(
-          (
-            { id, nome, preco, cores } // Seria o mesmo que .map((produto)), porém iria repetir produto toda vez que fosse chamar o id, nome preco...
-          ) => (
-            <div key={id}>
-              <h2>Produto: {nome}</h2>
-              <div>Preço: R$ {Number(preco.replace("R$ ", ""))}</div>
-              <ul>
-                {cores.map((cor) => (
-                  <li
-                    key={cor}
-                    style={{
-                      backgroundColor: cor,
-                      padding: ".5em",
-                      color: "#fff",
-                    }}
-                  >
-                    Cor de background: {cor.toUpperCase()}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )
-        )}
+      <div style={{ display: "flex", gap: "30px" }}>
+        <button onClick={handleClick}>Camiseta</button>
+        <button onClick={({ target }) => target.classList.toggle("perigo")}>
+          Muda de cor
+        </button>
+        <button onMouseMove={handleHover}>Hover me</button>
+      </div>
     </>
   );
 };
