68d8f0b00b6113af233a70f91f6ef35c89049ab1
Gabriel Henrique <ghbranco6@gmail.com>
2024-03-28 19:16:17 -0300

    🎉 First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Origamid | React completo
+
+- Este repositório contempla a resolução dos exercícios apresentados no curso de React Completo para Iniciantes produzido pela Origamid
+- Cada branch contém um exercício diferente, e cada commit contém, em sua descrição, o link para o exercício no site oficial da Origamid
+
+<img align="center" alt="React" height="50" width="50" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg">
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+// const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
+import "./style.scss";
+
+const App = () => {
+  return (
+    <>
+      <h1>
+        <a
+          target="_blank"
+          href="https://www.origamid.com/slide/react-completo/#/0405-checkbox/5"
+        >
+          Exercício Form - Checkbox | Slide 0405 ↗
+        </a>
+      </h1>
+    </>
+  );
+};
+
+export default App;
