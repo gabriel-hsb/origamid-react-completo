4d651f576e737e30551cdcf3915ac0b4ffaa3ac4
Gabriel Henrique <ghbranco6@gmail.com>
2024-03-27 21:42:00 -0300

    ⚡️ Perf (MyComponent) map for displaying inputs
    O exercício pediu para otimizar um código que tinham vários inputs com valores parecidos escritos um por um. Utilizando um array, faz-se o use do map para retornar todos os inputs a partir do array, sem precisar ficar se repetindo. caso precise adicionar mais inputs, é somente necessário alterar o array, e não os inputs (y)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+// const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
 
 import MyComponent from "./components/MyComponent";
-import { GlobalStorage } from "./GlobalContext";
 
 import "./style.scss";
 
@@ -18,15 +14,13 @@ const App = () => {
       <h1>
         <a
           target="_blank"
-          href="https://www.origamid.com/slide/react-completo/#/0305-usecontext/6"
+          href="https://www.origamid.com/slide/react-completo/#/0405-checkbox/5"
         >
-          Exercício useContext | Slide 0305 ↗
+          Exercício Form - Checkbox | Slide 0405 ↗
         </a>
       </h1>
 
-      <GlobalStorage>
-        <MyComponent  />
-      </GlobalStorage>
+      <MyComponent />
     </>
   );
 };
--- src/Components/MyComponent/index.jsx
+++ src/Components/MyComponent/index.jsx
@@ -1,53 +1,52 @@
-import { useContext } from "react";
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+// const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
+
+import { useState } from "react";
 
 import "./MyComponent.scss";
 
-import { GlobalContext } from "../../GlobalContext";
-import Button from "../Button";
-
 const MyComponent = () => {
-  const global = useContext(GlobalContext);
+  const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
+
+  const [cores, setCores] = useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
 
   return (
     <>
-      <h1>Produtos Ranek</h1>
-
-      <Button handleClick={global.handleClick} btnInner={"Limpar dados"} />
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          columnGap: "2rem",
-        }}
-      >
-        {global.produtos.map(({ id, nome, preco, descricao }) => (
-          <ul
-            style={{
-              marginBlock: "1.5rem",
-              backgroundColor: "#caf",
-              padding: "2rem",
-              borderRadius: "8px",
-            }}
-            key={id}
-          >
-            <li>
-              {" "}
-              <b>Nome do produto:</b> {nome}{" "}
-            </li>
-
-            <li>
-              <b>Preço do produto: </b> R${preco}{" "}
-            </li>
-            <li>
-              <b>Descrição do produto: </b> {descricao}{" "}
-            </li>
-            <li>
-              <b>Id do produto:</b> {id}{" "}
-            </li>
-          </ul>
+      <form>
+        {coresArray.map((item) => { //para cada item no array coresArray...
+          return (
+            <label key={item} htmlFor={item}>
+              <input
+                type="checkbox"
+                value={item}
+                checked={handleChecked(item)}
+                onChange={handleChange}
+              />
+              {item}
+            </label>
+          );
+        })}
+      </form>
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
         ))}
-      </div>
+      </ul>
     </>
   );
 };
